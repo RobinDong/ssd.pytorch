d662a9ed30d2f6e980c2ac688306b8add73557e4
Robin Dong <robin.k.dong@gmail.com>
2019-05-31 14:22:12 +0800

    Support CUB-200 and VOC2007/2012 dataset for detector.py

--- detector.py
+++ detector.py
@@ -1,15 +1,22 @@
 import cv2
 import time
 import torch
+import argparse
 
 from torch.autograd import Variable
 from ssd import build_ssd
-from data import BaseTransform
+from data import *
 
-def main():
+def main(args):
     # Load net
-    num_classes = 21
-    net = build_ssd('test', 300, num_classes)
+    if args.dataset == 'VOC':
+        cfg = voc
+        bird_index = 3
+    elif args.dataset == 'CUB':
+        cfg = cub
+        bird_index = 1
+
+    net = build_ssd('test', 300, cfg['num_classes'])
     ckpt = torch.load('weights/ssd300_COCO_120000.pth', map_location = 'cpu')
     net.load_state_dict(ckpt)
     net.eval()
@@ -28,11 +35,10 @@ def main():
     FONT = cv2.FONT_HERSHEY_SIMPLEX
 
     scale = torch.Tensor([width, height, width, height])
-    bird_index = 3
     j = 0
-    while detections[0, bird_index, j, 0] >= 0.5:
+    score = detections[0, bird_index, j, 0]
+    while score >= 0.5:
         pt = (detections[0, bird_index, j, 1:] * scale).cpu().numpy()
-        score = detections[0, bird_index, j, 0]
         print('pt', pt, score)
         cv2.rectangle(img,
                       (int(pt[0]), int(pt[1])),
@@ -41,9 +47,15 @@ def main():
         cv2.putText(img, 'bird_'+str(score.item()), (int(pt[0]), int(pt[1])),
                     FONT, 1, (255, 255, 255), 1, cv2.LINE_AA)
         j += 1
+        score = detections[0, bird_index, j, 0]
     print('time', time.time() - begin)
     print('objects', j)
 
     cv2.imwrite('draw.jpg', img)
 
-main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default='VOC', choices=['VOC', 'CUB'],
+                        type=str, help='VOC or CUB')
+    args = parser.parse_args()
+    main(args)
