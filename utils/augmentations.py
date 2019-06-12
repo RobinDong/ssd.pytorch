a0f194b0cdfcabfc8d4ff10e4efa9f1a0f506077
Robin Dong <robin.k.dong@gmail.com>
2019-06-12 11:16:10 +0800

    Adjust more brightness could detect all three birds in http://donghao.org/2019/05/31/using-single-shot-detection-to-detect-birds-episode-one/ for conf_thres=0.4

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -200,9 +200,8 @@ class RandomBrightness(object):
         self.delta = delta
 
     def __call__(self, image, boxes=None, labels=None):
-        if random.randint(2):
-            delta = random.uniform(-self.delta, self.delta)
-            image += delta
+        delta = random.uniform(-self.delta, self.delta)
+        image += delta
         return image, boxes, labels
 
 
