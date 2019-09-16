a8b54e920c24ea8b02c97443f30c8c5838b7f2ba
Robin Dong <robin.k.dong@gmail.com>
2019-09-16 00:00:53 +0000

    Use totally 200000 steps to train could reach 0.8 mean-AP for 2012-train in VOC

--- data/config.py
+++ data/config.py
@@ -13,8 +13,8 @@ MEANS = (104, 117, 123)
 # SSD300 CONFIGS
 voc = {
     'num_classes': 21,
-    'lr_steps': (20000, 40000, 60000, 80000),
-    'max_iter': 80001,
+    'lr_steps': (60000, 200000),
+    'max_iter': 200001,
     'feature_maps': [19, 10, 5, 3, 2, 1],
     'min_dim': 300,
     'steps': [16, 30, 60, 100, 150, 300],
--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python3.6 eval.py --trained_model=weights/ssd300_COCO_140000.pth --voc_root=/home/hdong/data/VOCdevkit
+python3.6 eval.py --trained_model=weights/ssd300_COCO_200000.pth --voc_root=/home/hdong/data/VOCdevkit --backbone=mobilenet --cuda=False
